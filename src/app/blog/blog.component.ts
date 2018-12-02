1366b491ad24a569c9a5ef732716b7c4517411b3
SGTMcClain <n8mcclain@gmail.com>
2018-12-02 15:33:54 -0500

    Added Azizi's Blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  // tslint:disable-next-line:max-line-length
+  blogText: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!'; 
+  aziziAuthor: string = 'Azizi Gibson';
+
   constructor() { }
 
   ngOnInit() {
