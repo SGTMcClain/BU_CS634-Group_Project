82d4678422f84e616360300ca6dce3df6322637c
SGTMcClain <n8mcclain@gmail.com>
2018-12-08 02:09:21 -0500

    Hide hamburger menu on click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +9,18 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('navbarToggler') navbarToggler:ElementRef;
+
   ngOnInit() {
   }
 
+  navBarTogglerIsVisible() {
+    return this.navbarToggler.nativeElement.offsetParent !== null;
+  }
+
+  collapseNav() {
+    if (this.navBarTogglerIsVisible()) {
+      this.navbarToggler.nativeElement.click();
+    }
+  }
 }
